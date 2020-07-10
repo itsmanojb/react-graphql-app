d831c93e47cccd070205b187fff462e306d03a37
itsmanojb <the_manoj@live.com>
2020-07-11 01:20:38 +0530

    added type-relations and more root-queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,21 +1,55 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList
+} = graphql;
 
 // dummy data
 const movies = [
-  { title: 'The Avengers', genre: 'Action/Sci-fi', id: '1' },
-  { title: 'The Avengers: Age of Ultron', genre: 'Action/Sci-fi', id: '2' },
-  { title: 'The Avengers: Infinity War', genre: 'Action/Sci-fi', id: '3' },
-  { title: 'The Avengers: Endgame', genre: 'Action/Sci-fi', id: '4' }
-]
+  { title: 'The Avengers', genre: 'Action/Sci-fi', id: '1', directorId: '1' },
+  { title: 'The Avengers: Age of Ultron', genre: 'Action/Sci-fi', id: '2', directorId: '1' },
+  { title: 'The Avengers: Infinity War', genre: 'Action/Sci-fi', id: '3', directorId: '2' },
+  { title: 'The Avengers: Endgame', genre: 'Action/Sci-fi', id: '4', directorId: '3' }
+];
+
+const directors = [
+  { name: 'Joss Whedon', age: 56, id: '1' },
+  { name: 'Anthony Russo', age: 50, id: '2' },
+  { name: 'Joe Russo', age: 48, id: '3' }
+];
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     title: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    director: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return directors.filter(director => director.id === parent.directorId)[0];
+      }
+    }
+  })
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: 'Director',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve(parent, args) {
+        return movies.filter(movie => movie.directorId === parent.id);
+      }
+    }
   })
 });
 
@@ -24,12 +58,30 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     movie: {
       type: MovieType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // code to get data from db / other source
         return movies.filter(movie => movie.id === args.id)[0];
       }
-    }
+    },
+    movies: {
+      type: new GraphQLList(MovieType),
+      resolve(parent, args) {
+        return movies;
+      }
+    },
+    director: {
+      type: DirectorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return directors.filter(director => director.id === args.id)[0];
+      }
+    },
+    directors: {
+      type: new GraphQLList(DirectorType),
+      resolve(parent, args) {
+        return directors;
+      }
+    },
   }
 });
 
