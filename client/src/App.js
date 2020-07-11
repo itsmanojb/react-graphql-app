05c168c04680d95fa6d5d35d56eb555f0208a915
itsmanojb <the_manoj@live.com>
2020-07-11 14:53:00 +0530

    :sparkles: Apollo client setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
-function App() {
-  return (
-    <div className="App">
+// Apollo client setup
+const client = new ApolloClient({
+  uri: `http://localhost:4000/graphql/`
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app <span role="img">🚀</span> </h2>
     </div>
-  );
-}
+  </ApolloProvider>
+);
 
 export default App;
